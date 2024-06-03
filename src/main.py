536eae3a848eac392aac068f7ec2d8e6109f0b78
aivanns <slavchonok11@gmail.com>
2024-06-03 14:19:57 +0300

    123

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         app='app.app:app',
-        host='127.0.0.1',
-        port=8090,
+        host='0.0.0.0',
+        port=8080,
         reload=True
     )
 
